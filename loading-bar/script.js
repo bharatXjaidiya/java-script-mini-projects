3ffc2c30627798f0521d8f606a0c0280c1699bdd
bharatXjaidiya <jaidiyabharat16@gmail.com>
2026-01-28 05:37:13 +0530

    logic done

--- loading-bar/script.js
+++ loading-bar/script.js
@@ -1,16 +1,17 @@
 let loader = document.querySelector(".loader");
 let btn = document.querySelector("button");
 let width = 0;
-btn.addEventListener('click',(e)=>{
-    btn.removeEventListener('click');
+btn.addEventListener('click', (e) => {
+    let r = Math.floor(Math.random() * 14) + 7;
+    let des = document.querySelector(".des");
+    des.innerHTML = `your content will be downloaded in <b>${r}</b> sec...`
     width = 0;
-    let w = setInterval((e)=>{
+    let w = setInterval((e) => {
         ++width;
         console.log(width)
         loader.style.width = width + "%";
-
-        if(width > 100){
+        if (width >= 100) {
             clearInterval(w);
         }
-    },10)
+    }, r * 10)
 })
